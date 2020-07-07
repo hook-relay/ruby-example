4cdbfe3deae111b87e61f1fe6fad3696c4d226c1
Benjamin Curtis <ben@bencurtis.com>
2020-07-07 11:56:47 -0700

    Simplify -- and actually send the body :)

--- app.rb
+++ app.rb
@@ -9,18 +9,13 @@ end
 post "/" do
   logger.info "Posting to #{params[:url]} via #{ENV["HOOKRELAY_URL"]}"
 
-  uri = URI(ENV["HOOKRELAY_URL"])
-  req = Net::HTTP::Post.new(uri)
-
-  params[:headers].split(/\r?\n/).each do |pair|
+  headers = params[:headers].split(/\r?\n/).each_with_object({}) { |pair, h|
     k, v = pair.split(/:\s*/)
-    req[k] = v
-  end
-  req["HR-Target-Url"] = params[:url]
+    h[k] = v
+  }.merge("HR-Target-Url" => params[:url])
 
-  res = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http|
-    http.request(req)
-  }
+  res = Net::HTTP.post(URI(ENV["HOOKRELAY_URL"]), params[:body], headers)
+
   logger.info "Reponse: #{res.inspect} / #{res["location"]}"
 
   redirect "/?location=#{res["Location"]}", 302
